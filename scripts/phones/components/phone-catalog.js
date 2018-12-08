a950debae34bad522c7adb29c010766cb8981c6a
Dmitriy Ovsyannikov <ovsyannikov.dmitry@gmail.com>
2018-12-09 00:10:54 +0500

    Implement phone viewer of one phone

--- scripts/phones/components/phone-catalog.js
+++ scripts/phones/components/phone-catalog.js
@@ -1,14 +1,18 @@
-export default class PhoneCatalog {
-	constructor({element, phones}) {
+import Component from '../../component.js';
+
+export default class PhoneCatalog extends Component {
+	constructor({element, phones, onPhoneSelected}) {
+		super({element});
 		this._element = element;
 		this._phones = phones;
+		this._onPhoneSelected = onPhoneSelected;
 		this._render();
 		this._element.addEventListener('click', (event) => {
 			let phoneElement = event.target.closest('[data-element="phone-item"]');
 			if (!phoneElement) {
 				return;
 			}
-			console.log(phoneElement.dataset.phoneId);
+			this._onPhoneSelected(phoneElement.dataset.phoneId);
 		})
 	}
 
@@ -18,7 +22,7 @@ export default class PhoneCatalog {
 	      ${this._phones.map(phone => `
 	      	<li class="thumbnail" data-element="phone-item" data-phone-id=${phone.id}>
 	          <a href="#${phone.id}" class="thumb">
-	            <img alt="${phone.name}">
+	            <img alt="${phone.name}" src="${phone.imageUrl}">
 	          </a>
 
 	          <div class="phones__btn-buy-wrapper">
--- scripts/phones/phones-page.js
+++ scripts/phones/phones-page.js
@@ -1,4 +1,5 @@
 import PhoneCatalog from './components/phone-catalog.js';
+import PhoneViewer from './components/phone-viewer.js';
 import PhoneService from './phone-service.js';
 
 export default class PhonesPage {
@@ -6,9 +7,18 @@ export default class PhonesPage {
 		this._element = element;
 		this._render();
 
-		new PhoneCatalog({
+		this._catalog = new PhoneCatalog({
 			element: this._element.querySelector('[data-component="phone-catalog"]'),
 			phones: PhoneService.getAll(),
+			onPhoneSelected: (phoneId) => {
+				const phoneDetails = PhoneService.getOneById(phoneId);
+				this._catalog.hide();
+				this._viewer.show(phoneDetails);
+			}
+		});
+
+		this._viewer = new PhoneViewer({
+			element: this._element.querySelector('[data-component="phone-viewer"]'),
 		});
 	}
 
@@ -46,6 +56,7 @@ export default class PhonesPage {
 	      <!--Main content-->
 	      <div class="col-md-10">
 	      	<div data-component="phone-catalog"></div>
+	      	<div data-component="phone-viewer"></div>
 	      </div>
 
 	    </div>
