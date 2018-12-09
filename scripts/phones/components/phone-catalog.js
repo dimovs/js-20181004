beea441bc0e0c4c41544d4e3b5b0fad57f9493c7
Dmitriy Ovsyannikov <ovsyannikov.dmitry@gmail.com>
2018-12-09 13:03:17 +0500

    Add switch of image gallery

--- scripts/phones/components/phone-catalog.js
+++ scripts/phones/components/phone-catalog.js
@@ -7,11 +7,20 @@ export default class PhoneCatalog extends Component {
 		this._onPhoneSelected = onPhoneSelected;
 		this._render();
 		this._element.addEventListener('click', (event) => {
-			let phoneElement = event.target.closest('[data-element="phone-item"]');
-			if (!phoneElement) {
+			const phoneLink = event.target.closest('[data-element="phone-link"]');
+			if (!phoneLink) {
 				return;
 			}
+			const phoneElement = event.target.closest('[data-element="phone-item"]');
 			this._onPhoneSelected(phoneElement.dataset.phoneId);
+		});
+		this._element.addEventListener('click', (event) => {
+			const button = event.target.closest('[data-element="add-button"]');
+			if (!button) {
+				return;
+			}
+			const phoneElement = event.target.closest('[data-element="phone-item"]');
+			phoneElement.dataset.phoneId;
 		})
 	}
 
@@ -20,17 +29,17 @@ export default class PhoneCatalog extends Component {
       <ul class="phones">
 	      ${this._phones.map(phone => `
 	      	<li class="thumbnail" data-element="phone-item" data-phone-id=${phone.id}>
-	          <a href="#${phone.id}" class="thumb">
+	          <a href="#${phone.id}" class="thumb" data-element="phone-link">
 	            <img alt="${phone.name}" src="${phone.imageUrl}">
 	          </a>
 
 	          <div class="phones__btn-buy-wrapper">
-	            <a class="btn btn-success">
+	            <a class="btn btn-success" data-element="add-button">
 	              Add
 	            </a>
 	          </div>
 
-	          <a href="#${phone.id}">${phone.name}</a>
+	          <a href="#${phone.id}" data-element="phone-link">${phone.name}</a>
 	          <p>${phone.snippet}</p>
 	        </li>
 	      `).join('')}
--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -1,43 +1,62 @@
 import Component from '../../component.js';
 
 export default class PhoneViewer extends Component {
+	constructor({element, onAdd, onBack}) {
+		super({element});
+		this._onAdd = onAdd;
+		this._onBack = onBack;
+
+		this._element.addEventListener('click', (event) => {
+			const smallImage = event.target.closest('[data-element="small-image"]');
+			if (!smallImage) {
+				return;
+			}
+			this._currentImage = smallImage.src;
+			this._render();
+		});
+
+		this._element.addEventListener('click', (event) => {
+			const button = event.target.closest('[data-element="back-button"]');
+			if (!button) {
+				return;
+			}
+			this._onBack();
+		});
+
+		this._element.addEventListener('click', (event) => {
+			const button = event.target.closest('[data-element="add-button"]');
+			if (!button) {
+				return;
+			}
+			this._onAdd(this._phone.id);
+		});
+	}
+
 	show(phone) {
 		this._phone = phone;
+		this._currentImage = phone.images[0];
 		this._render();
 		super.show();
 	}
 
 	_render() {
+		const {name, description, images} = this._phone;
 		this._element.innerHTML = `
-	    <img class="phone" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+	    <img class="phone" src="${this._currentImage}">
 
-	    <button>Back</button>
-	    <button>Add to basket</button>
+	    <button data-element="back-button">Back</button>
+	    <button data-element="add-button">Add to basket</button>
 
+	    <h1>${name}</h1>
 
-	    <h1>Motorola XOOM™ with Wi-Fi</h1>
-
-	    <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
+	    <p>${description}</p>
 
 	    <ul class="phone-thumbs">
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-	      </li>
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-	      </li>
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-	      </li>
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-	      </li>
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-	      </li>
-	      <li>
-	        <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-	      </li>
+	    	${images.map(image => `
+		      <li>
+		        <img src="${image}" data-element="small-image">
+		      </li>
+	    	`).join('')}
 	    </ul>
 		`;
 	}
